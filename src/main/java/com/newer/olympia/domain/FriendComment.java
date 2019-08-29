2a0540035371f639121be20f80be019040e41e2f
yangdu1997 <562063683@qq.com>
2019-08-29 23:00:15 +0800

    commint file5

---
+++ src/main/java/com/newer/olympia/domain/FriendComment.java
@@ -0,0 +1,100 @@
+package com.newer.olympia.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.io.Serializable;
+import java.util.Date;
+//封装了页面上评论的显示内容
+public class FriendComment implements Serializable {
+
+    private static final long serialVersionUID = 5722808533739073423L;
+    private Integer Comment_id;//id
+    private Integer Comment_user_id;//用户评论id
+    private Integer Blogs_id;//评论的动态或博文的ad
+    private String Comment_content;//评论的内容
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    private Date Comment_time;//评论时间
+    private Integer User_id;//用户id
+    private String Comment_user_name;//评论好友的名字
+    private String Comment_user_img;//评论好友头像
+
+    public FriendComment() {
+    }
+
+    public FriendComment(Integer comment_id, Integer comment_user_id, Integer blogs_id, String comment_content, Date comment_time, Integer user_id, String comment_user_name, String comment_user_img) {
+        Comment_id = comment_id;
+        Comment_user_id = comment_user_id;
+        Blogs_id = blogs_id;
+        Comment_content = comment_content;
+        Comment_time = comment_time;
+        User_id = user_id;
+        Comment_user_name = comment_user_name;
+        Comment_user_img = comment_user_img;
+    }
+
+    public Integer getComment_id() {
+        return Comment_id;
+    }
+
+    public void setComment_id(Integer comment_id) {
+        Comment_id = comment_id;
+    }
+
+    public Integer getComment_user_id() {
+        return Comment_user_id;
+    }
+
+    public void setComment_user_id(Integer comment_user_id) {
+        Comment_user_id = comment_user_id;
+    }
+
+    public Integer getBlogs_id() {
+        return Blogs_id;
+    }
+
+    public void setBlogs_id(Integer blogs_id) {
+        Blogs_id = blogs_id;
+    }
+
+    public String getComment_content() {
+        return Comment_content;
+    }
+
+    public void setComment_content(String comment_content) {
+        Comment_content = comment_content;
+    }
+
+    public Date getComment_time() {
+        return Comment_time;
+    }
+
+    public void setComment_time(Date comment_time) {
+        Comment_time = comment_time;
+    }
+
+    public Integer getUser_id() {
+        return User_id;
+    }
+
+    public void setUser_id(Integer user_id) {
+        User_id = user_id;
+    }
+
+    public String getComment_user_name() {
+        return Comment_user_name;
+    }
+
+    public void setComment_user_name(String comment_user_name) {
+        Comment_user_name = comment_user_name;
+    }
+
+    public String getComment_user_img() {
+        return Comment_user_img;
+    }
+
+    public void setComment_user_img(String comment_user_img) {
+        Comment_user_img = comment_user_img;
+    }
+}
---
+++ src/main/java/com/newer/olympia/service/ProfilePageServiceImpl.java
@@ -0,0 +1,52 @@
+package com.newer.olympia.service;
+
+import com.newer.olympia.domain.*;
+import com.newer.olympia.mapper.ProfilePageMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class ProfilePageServiceImpl implements ProfilePageService{
+    @Autowired
+    private ProfilePageMapper profilePageMapper;
+
+    @Override
+    public List<Blogs> selectBlogsAllByid(int User_id) {
+        return profilePageMapper.selectBlogsAllByid(User_id);
+    }
+
+    @Override
+    public List<FriendComment> selectCommentAllByid(int User_id,int Blogs_id) {
+        return profilePageMapper.selectCommentAllByid(User_id,Blogs_id);
+    }
+
+    @Override
+    public int selectCommentCount(int User_id, int Blogs_id) {
+        return profilePageMapper.selectCommentCount(User_id,Blogs_id);
+    }
+
+    @Override
+    public String selectDescribe(int User_id) {
+        return profilePageMapper.selectDescribe(User_id);
+    }
+
+    @Override
+    public Hobbies selectHobbies(int User_id) {
+        return profilePageMapper.selectHobbies(User_id);
+    }
+
+    @Override
+    public List<Apply> selectApply(int User_id) {
+        return profilePageMapper.selectApply(User_id);
+    }
+
+    @Override
+    public User selectUser(int User_id) {
+        return profilePageMapper.selectUser(User_id);
+    }
+
+
+
+
+}
